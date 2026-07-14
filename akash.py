d2a71ca4693832952ac6963c3020117afb06f652
akash9159 <clazieakash@gmail.com>
2026-07-14 15:46:31 +0530

    added new message

--- akash.py
+++ akash.py
@@ -1,4 +1,7 @@
+homies = ['akash', 'anas', 'ashish','avinash']
 print('Hello, World!')
 print('This is a sample Python script.')
 for i in range(3):
     print(f'akash {i + 1}')
+if 'akash' in homies:
+    print('We are friends!')
